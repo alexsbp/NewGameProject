6d1d78f6f1d0d87bf3a9d4039ecdec084f035a86
S <S@LENOVO-NOVUS>
2017-03-16 13:29:16 +0100

    d

--- NewGameProjekt/NewGameProjekt/Program.cs
+++ NewGameProjekt/NewGameProjekt/Program.cs
@@ -16,7 +16,7 @@ namespace NewGameProjekt
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1());  
         }
     }
 }
